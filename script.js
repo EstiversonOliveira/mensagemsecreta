d3c77a1f765e54185629e31d9164b80dd35546fa
Estíverson Oliveira <107802352+EstiversonOliveira@users.noreply.github.com>
2024-09-24 16:59:48 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-
-function exibirAlerta() {
-    const mensagemSecreta = "Ao ser confrontado o zelador confessou que o roubou para proteger seu conteúdo valioso antes que a biblioteca passasse por uma atualização que poderia apagá-lo. deveriam eles explorar essa nova descoberta ou informar as autoridades sobre o roubo? A escolha poderia alterar o destino do zelador e o futuro da biblioteca para sempre.";
-    const elemento = document.querySelector(".texto");
-    elemento.classList.add("texto-secreto");
-    elemento.innerHTML = mensagemSecreta;
-    document.querySelector("button").classList.add("remove");
-}
